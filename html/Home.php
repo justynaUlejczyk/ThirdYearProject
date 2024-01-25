4ec6c481549bf1c2c49d3d1e3baaf07d1442c2a4
kg2020 <kg2020@hw.ac.uk>
2024-01-25 22:59:34 +0000

    split js code into 2 files as some code will be reused and therefore put in main.js

--- html/Home.php
+++ html/Home.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
         integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <script src="../js/Home.js"></script>
+    <script src="../js/main.js"></script>
 </head>
 
 <!-- test commit -->
---
+++ js/main.js
@@ -0,0 +1,63 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    dropdownContent.classList.toggle("active");
+}
+
+
+function toggleHeart(button) {
+    button.classList.toggle("active");
+}
+
+
+function exitButton() {
+    var postPreElement = document.querySelector('prepost');
+    var dimmedElement = document.querySelector('.dimmed');
+    var createPost = document.querySelector('.feed-create-post-container')
+
+    dimmedElement.classList.remove('active');
+    postPreElement.style.display = 'none';
+    createPost.style.display = 'none';
+}
+
+function openPost(clickedElement) {
+    // Find the closest post element by using querySelector
+    var postElement = clickedElement.closest('.posts');
+    var postPreElement = document.querySelector('prepost');
+    var dimmedElement = document.querySelector('.dimmed');
+
+    dimmedElement.classList.add('active');
+    postPreElement.style.display = 'flex';
+
+
+    // Check if a post element was found
+    if (postElement) {
+        // Get the ID attribute of the post element
+        var postId = postElement.getAttribute('id');
+
+        // Display the information for the opened post
+        displayPostInfo(postId);
+
+        // Additional logic for handling the opening of the post can go here
+    } else {
+        console.log('No post element found.');
+    }
+}
+
+
+
+function toggleMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
