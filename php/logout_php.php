98448d43f2e3ab1350cd22fea44e79f727fba41f
jku2000@hw.ac.uk <jku2000@hw.ac.uk>
2024-01-29 13:53:19 +0000

    logout

--- php/logout_php.php
+++ php/logout_php.php
@@ -1,37 +1,21 @@
 <?php
 session_start();
-    if (!isset($_SESSION["username"])) {
-        header('Location: ' . "./login.php");
-    }
 
-    require_once "../php/connect_db.php";
+if (!isset($_SESSION["username"])) {
+    header('Location: ./login.php');
+    exit(); // Make sure to exit after redirection to prevent further execution
+}
 
-    $username = $_SESSION["username"];
-    
+require_once "../php/connect_db.php";
+
+$username = $_SESSION["username"];
 
 # Clear existing variables.
-$_SESSION = array() ;
+$_SESSION = array();
 # Destroy the session.
-session_destroy() ;
-# Display body section.
+session_destroy();
 
-echo '<br> <div class="container text-center">
- <div class ="row" >
-	<div class="col align-self-center">
-	<div class = "col-md-6 offset-md-3"  >
-	<div class="card-header">
-	<div class="text-center">
-	<div class = "card-header"> <h5> GOOD BYE!</h5><br>
-    <h4 class="card-subtitle mb-2 text-muted">You are now logged out.</h4>
-   
-    <a href="../html/Login.php">Login</a>
-    </div>
-	</div>
-	</div>
-  </div>
-</div>
-</div>
-' ;
- 
-
+# Redirect to the login page.
+header('Location: ../html/login.php');
+exit(); // Make sure to exit after redirection to prevent further execution
 ?>
