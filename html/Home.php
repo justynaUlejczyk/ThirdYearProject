a7db8018d4f83a89c4f4288c98026a4e572ca577
kg2020 <kg2020@hw.ac.uk>
2024-01-29 09:34:52 +0000

    edited couple stuff for css and html as well as finally changed cat img to unknown profile

--- html/Home.php
+++ html/Home.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
-    if (!isset($_SESSION["username"])) {
-        header('Location: ' . "./login.php");
-    }
+if (!isset($_SESSION["username"])) {
+    header('Location: ' . "./login.php");
+}
 
-    require_once "../php/connect_db.php";
+require_once "../php/connect_db.php";
 
-    $username = $_SESSION["username"];
+$username = $_SESSION["username"];
 
-    $userDataSTMT = pg_prepare($conn, "user_data", "SELECT * FROM accounts where username = $1");
-    $userDataRESULT = pg_execute($conn, "user_data", array($username));
-    $name = pg_fetch_result($userDataRESULT, 0, "name");
+$userDataSTMT = pg_prepare($conn, "user_data", "SELECT * FROM accounts where username = $1");
+$userDataRESULT = pg_execute($conn, "user_data", array($username));
+$name = pg_fetch_result($userDataRESULT, 0, "name");
 ?>
 
 <!DOCTYPE html>
@@ -39,7 +39,7 @@ session_start();
         <ul>
             <li>
                 <a href="Profile.php">
-                    <img src="../images/cat.jpg" class="nav-profile">
+                    <img src="../images/icons/Unknown_person.jpg" class="nav-profile">
                 </a>
             </li>
 
@@ -161,7 +161,7 @@ session_start();
                 </li>
 
                 <a href="../html/Profile.php">
-                    <img class="nav-profile" src="../images/cat.jpg" />
+                    <img class="nav-profile" src="../images/icons/Unknown_person.jpg" />
                 </a>
 
             </ul>
@@ -178,8 +178,8 @@ session_start();
             <div>
                 <div>
                     <div id="Banner"></div>
-                    <a href="Profile.php"><img class="pfp" src="../images/cat.jpg" alt="pfp" width="100"
-                            height="100" /></a>
+                    <a href="Profile.php"><img class="pfp" src="../images/icons/Unknown_person.jpg" alt="pfp"
+                            width="100" height="100" /></a>
                     <div id="pfp-outline"></div>
                 </div>
 
@@ -216,10 +216,11 @@ session_start();
                 <div class="feed-create-post-container">
                     <a href="#" class="close" onclick="exitButton(this)"></a>
                     <form action="../php/create_post.php" method="post" enctype="multipart/form-data">
-                        <input type="file" class="feed-create-post-img" name ="post_image" accept=".png, .jpg, .jpeg">
+                        <input type="file" class="feed-create-post-img" name="post_image" accept=".png, .jpg, .jpeg">
                         <div>
                             <h3>Caption</h3>
-                            <textarea id="text" name="text" rows="4" cols="50" maxlength="3000" class="feed-create-post-captions"></textarea> 
+                            <textarea id="text" name="text" rows="4" cols="50" maxlength="3000"
+                                class="feed-create-post-captions"></textarea>
                         </div>
                         <div>
                             <h3>tags will go here</h3>
@@ -254,7 +255,7 @@ session_start();
                     $postLikedByUserRESULT = pg_execute($conn, "postLikedByUser", array($postid, $username));
                     $postLikedByUser = pg_num_rows($postLikedByUserRESULT) != 0;
 
-            echo "<post class='posts' id=$postid>";
+                    echo "<post class='posts' id=$postid>";
                     echo " <prepost>
             <div class='post-column'>
                 <div class='expanded-post'>
@@ -267,7 +268,7 @@ session_start();
                 <div class='post-info'>
                     <div class=' comment-header'>
                         <div class='user-container'>
-                            <a href='Profile.php'><img src='../images/cat.jpg' class='post-avatar' /></a>
+                            <a href='Profile.php'><img src='../images/icons/Unknown_person.jpg' class='post-avatar' /></a>
                             <div class='user-post-name'>
                                 <span>$name</span>
                                 <span>@$poster_username</span>
@@ -280,10 +281,10 @@ session_start();
                             <!-- Likes -->";
                     if ($postLikedByUser) {
                         echo "<button id='likePostButton' class='like icons $postid active' onclick='toggleHeart(this);handleLikeButtonClick($postid);' >";
-                    } else{
+                    } else {
                         echo "<button id='likePostButton' class='like icons $postid' onclick='toggleHeart(this);handleLikeButtonClick($postid);' >";
                     }
-                            echo   "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'
+                    echo "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'
                                     xmlns='http://www.w3.org/2000/svg'>
                                     <path
                                         d='M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z'
@@ -330,7 +331,7 @@ session_start();
                 <div class='comment-container'>
                     <div class='comment-user-comment'>
                         <div class='user-container'>
-                            <a href='Profile.php'><img src='../images/cat.jpg' class='post-avatar' /></a>
+                            <a href='Profile.php'><img src='../images/icons/Unknown_person.jpg' class='post-avatar' /></a>
                             <div class='user-post-name'>
                                 <span>Michael Schumacher</span>
                                 <span>Comment - 22/01/23</span>
@@ -361,7 +362,7 @@ session_start();
                     </div>
                     <div class='comment-user-comment'>
                         <div class='user-container'>
-                            <a href='Profile.php'><img src='../images/cat.jpg' class='post-avatar' /></a>
+                            <a href='Profile.php'><img src='../images/icons/Unknown_person.jpg' class='post-avatar' /></a>
                             <div class='user-post-name'>
                                 <span>Michael Schumacher</span>
                                 <span>Comment - 22/01/23</span>
@@ -391,7 +392,7 @@ session_start();
                     </div>
                     <div class='comment-user-comment'>
                         <div class='user-container'>
-                            <a href='Profile.php'><img src='../images/cat.jpg' class='post-avatar' /></a>
+                            <a href='Profile.php'><img src='../images/icons/Unknown_person.jpg' class='post-avatar' /></a>
                             <div class='user-post-name'>
                                 <span>Michael Schumacher</span>
                                 <span>Comment - 22/01/23</span>
@@ -421,7 +422,7 @@ session_start();
                     </div>
                     <div class='comment-user-comment'>
                         <div class='user-container'>
-                            <a href='Profile.php'><img src='../images/cat.jpg' class='post-avatar' /></a>
+                            <a href='Profile.php'><img src='../images/icons/Unknown_person.jpg' class='post-avatar' /></a>
                             <div class='user-post-name'>
                                 <span>Michael Schumacher</span>
                                 <span>Comment - 22/01/23</span>
@@ -452,7 +453,7 @@ session_start();
                     </div>
                     <div class='comment-user-comment'>
                         <div class='user-container'>
-                            <a href='Profile.php'><img src='../images/cat.jpg' class='post-avatar' /></a>
+                            <a href='Profile.php'><img src='../images/icons/Unknown_person.jpg' class='post-avatar' /></a>
                             <div class='user-post-name'>
                                 <span>Michael Schumacher</span>
                                 <span>Comment - 22/01/23</span>
@@ -483,7 +484,7 @@ session_start();
                     <div class='comment-replies'>
                         <div class='comment-user-comment'>
                             <div class='user-container'>
-                                <a href='Profile.php'><img src='../images/cat.jpg' class='post-avatar' /></a>
+                                <a href='Profile.php'><img src='../images/icons/Unknown_person.jpg' class='post-avatar' /></a>
                                 <div class='user-post-name'>
                                     <span>Michael Schumacher</span>
                                     <span>Comment - 22/01/23</span>
@@ -528,60 +529,60 @@ session_start();
                 </div>
             </div>
         </prepost>";
-            echo    "<div class='feed-post'>";
-            echo        "<div class='user-container'>";
-            echo            "<a href='Profile.php'><img src='../images/cat.jpg' class='post-avatar' /></a>";
-            echo            "<div class='user-post-name'>";
-            echo                "<span>$name</span>";
-            echo                "<span>@$poster_username</span>";
-            echo            "</div>";
-            echo        "</div>";
-            echo        "<button class='post-image' onclick='openPost(this)' data-postid=$postid>";
-            echo            "<img class='post-image' src=$post_image_path />";
-            echo        "</button>";
-            echo        "<div class='choices'>";
-            echo            "<div class='post-options'>";
-            echo                "<!-- Likes -->";
-            echo                "<button class='like icons' onclick='toggleHeart(this)'>";
-            echo                    "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'";
-            echo                        "xmlns='http://www.w3.org/2000/svg'>";
-            echo                        "<path";
-            echo                            ' d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"';
-            echo                            "fill='red' />";
-            echo                    "</svg>";
-            echo                "</button>";
-            echo                "<!-- Comment -->";
-            echo                "<button class='post-comment-button'>";
-            echo                    "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'";
-            echo                        "xmlns='http://www.w3.org/2000/svg'>";
-            echo                        "<path fill-rule='evenodd' clip-rule='evenodd'";
-            echo                            " d='M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22ZM8 13.25C7.58579 13.25 7.25 13.5858 7.25 14C7.25 14.4142 7.58579 14.75 8 14.75H13.5C13.9142 14.75 14.25 14.4142 14.25 14C14.25 13.5858 13.9142 13.25 13.5 13.25H8ZM7.25 10.5C7.25 10.0858 7.58579 9.75 8 9.75H16C16.4142 9.75 16.75 10.0858 16.75 10.5C16.75 10.9142 16.4142 11.25 16 11.25H8C7.58579 11.25 7.25 10.9142 7.25 10.5Z'";
-            echo                            "fill='#1C274C' />";
-            echo                    "</svg>";
-            echo                "</button>";
-            echo                "<!-- Send -->";
-            echo                "<button>";
-            echo                    "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'";
-            echo                        "xmlns='http://www.w3.org/2000/svg'>";
-            echo                        "<path";
-            echo                            " d='M18.6357 15.6701L20.3521 10.5208C21.8516 6.02242 22.6013 3.77322 21.414 2.58595C20.2268 1.39869 17.9776 2.14842 13.4792 3.64788L8.32987 5.36432C4.69923 6.57453 2.88392 7.17964 2.36806 8.06698C1.87731 8.91112 1.87731 9.95369 2.36806 10.7978C2.88392 11.6852 4.69923 12.2903 8.32987 13.5005C8.77981 13.6505 9.28601 13.5434 9.62294 13.2096L15.1286 7.75495C15.4383 7.44808 15.9382 7.45041 16.245 7.76015C16.5519 8.06989 16.5496 8.56975 16.2398 8.87662L10.8231 14.2432C10.4518 14.6111 10.3342 15.1742 10.4995 15.6701C11.7097 19.3007 12.3148 21.1161 13.2022 21.6319C14.0463 22.1227 15.0889 22.1227 15.933 21.6319C16.8204 21.1161 17.4255 19.3008 18.6357 15.6701Z'";
-            echo                            "fill='#1C274C' />";
-            echo                    "</svg>";
-            echo                "</button>";
-            echo            "</div>";
-            echo            "<!-- Pin -->";
-            echo            "<button>";
-            echo                "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'";
-            echo                    "xmlns='http://www.w3.org/2000/svg'>";
-            echo                    "<path";
-            echo                        " d='M19.1835 7.80516L16.2188 4.83755C14.1921 2.8089 13.1788 1.79457 12.0904 2.03468C11.0021 2.2748 10.5086 3.62155 9.5217 6.31506L8.85373 8.1381C8.59063 8.85617 8.45908 9.2152 8.22239 9.49292C8.11619 9.61754 7.99536 9.72887 7.86251 9.82451C7.56644 10.0377 7.19811 10.1392 6.46145 10.3423C4.80107 10.8 3.97088 11.0289 3.65804 11.5721C3.5228 11.8069 3.45242 12.0735 3.45413 12.3446C3.45809 12.9715 4.06698 13.581 5.28476 14.8L6.69935 16.2163L2.22345 20.6964C1.92552 20.9946 1.92552 21.4782 2.22345 21.7764C2.52138 22.0746 3.00443 22.0746 3.30236 21.7764L7.77841 17.2961L9.24441 18.7635C10.4699 19.9902 11.0827 20.6036 11.7134 20.6045C11.9792 20.6049 12.2404 20.5358 12.4713 20.4041C13.0192 20.0914 13.2493 19.2551 13.7095 17.5825C13.9119 16.8472 14.013 16.4795 14.2254 16.1835C14.3184 16.054 14.4262 15.9358 14.5468 15.8314C14.8221 15.593 15.1788 15.459 15.8922 15.191L17.7362 14.4981C20.4 13.4973 21.7319 12.9969 21.9667 11.9115C22.2014 10.826 21.1954 9.81905 19.1835 7.80516Z'";
-            echo                       "fill='#1C274C' />";
-            echo                "</svg>";
-            echo            "</button>";
-            echo        "</div>";
-            echo    "</div>";
+                    echo "<div class='feed-post'>";
+                    echo "<div class='user-container'>";
+                    echo "<a href='Profile.php'><img src='../images/icons/Unknown_person.jpg' class='post-avatar' /></a>";
+                    echo "<div class='user-post-name'>";
+                    echo "<span>$name</span>";
+                    echo "<span>@$poster_username</span>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "<button class='post-image' onclick='openPost(this)' data-postid=$postid>";
+                    echo "<img class='post-image' src=$post_image_path />";
+                    echo "</button>";
+                    echo "<div class='choices'>";
+                    echo "<div class='post-options'>";
+                    echo "<!-- Likes -->";
+                    echo "<button class='like icons' onclick='toggleHeart(this)'>";
+                    echo "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'";
+                    echo "xmlns='http://www.w3.org/2000/svg'>";
+                    echo "<path";
+                    echo ' d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"';
+                    echo "fill='red' />";
+                    echo "</svg>";
+                    echo "</button>";
+                    echo "<!-- Comment -->";
+                    echo "<button class='post-comment-button'>";
+                    echo "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'";
+                    echo "xmlns='http://www.w3.org/2000/svg'>";
+                    echo "<path fill-rule='evenodd' clip-rule='evenodd'";
+                    echo " d='M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22ZM8 13.25C7.58579 13.25 7.25 13.5858 7.25 14C7.25 14.4142 7.58579 14.75 8 14.75H13.5C13.9142 14.75 14.25 14.4142 14.25 14C14.25 13.5858 13.9142 13.25 13.5 13.25H8ZM7.25 10.5C7.25 10.0858 7.58579 9.75 8 9.75H16C16.4142 9.75 16.75 10.0858 16.75 10.5C16.75 10.9142 16.4142 11.25 16 11.25H8C7.58579 11.25 7.25 10.9142 7.25 10.5Z'";
+                    echo "fill='#1C274C' />";
+                    echo "</svg>";
+                    echo "</button>";
+                    echo "<!-- Send -->";
+                    echo "<button>";
+                    echo "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'";
+                    echo "xmlns='http://www.w3.org/2000/svg'>";
+                    echo "<path";
+                    echo " d='M18.6357 15.6701L20.3521 10.5208C21.8516 6.02242 22.6013 3.77322 21.414 2.58595C20.2268 1.39869 17.9776 2.14842 13.4792 3.64788L8.32987 5.36432C4.69923 6.57453 2.88392 7.17964 2.36806 8.06698C1.87731 8.91112 1.87731 9.95369 2.36806 10.7978C2.88392 11.6852 4.69923 12.2903 8.32987 13.5005C8.77981 13.6505 9.28601 13.5434 9.62294 13.2096L15.1286 7.75495C15.4383 7.44808 15.9382 7.45041 16.245 7.76015C16.5519 8.06989 16.5496 8.56975 16.2398 8.87662L10.8231 14.2432C10.4518 14.6111 10.3342 15.1742 10.4995 15.6701C11.7097 19.3007 12.3148 21.1161 13.2022 21.6319C14.0463 22.1227 15.0889 22.1227 15.933 21.6319C16.8204 21.1161 17.4255 19.3008 18.6357 15.6701Z'";
+                    echo "fill='#1C274C' />";
+                    echo "</svg>";
+                    echo "</button>";
+                    echo "</div>";
+                    echo "<!-- Pin -->";
+                    echo "<button>";
+                    echo "<svg width='24px' height='24px' viewBox='0 0 24 24' fill='none'";
+                    echo "xmlns='http://www.w3.org/2000/svg'>";
+                    echo "<path";
+                    echo " d='M19.1835 7.80516L16.2188 4.83755C14.1921 2.8089 13.1788 1.79457 12.0904 2.03468C11.0021 2.2748 10.5086 3.62155 9.5217 6.31506L8.85373 8.1381C8.59063 8.85617 8.45908 9.2152 8.22239 9.49292C8.11619 9.61754 7.99536 9.72887 7.86251 9.82451C7.56644 10.0377 7.19811 10.1392 6.46145 10.3423C4.80107 10.8 3.97088 11.0289 3.65804 11.5721C3.5228 11.8069 3.45242 12.0735 3.45413 12.3446C3.45809 12.9715 4.06698 13.581 5.28476 14.8L6.69935 16.2163L2.22345 20.6964C1.92552 20.9946 1.92552 21.4782 2.22345 21.7764C2.52138 22.0746 3.00443 22.0746 3.30236 21.7764L7.77841 17.2961L9.24441 18.7635C10.4699 19.9902 11.0827 20.6036 11.7134 20.6045C11.9792 20.6049 12.2404 20.5358 12.4713 20.4041C13.0192 20.0914 13.2493 19.2551 13.7095 17.5825C13.9119 16.8472 14.013 16.4795 14.2254 16.1835C14.3184 16.054 14.4262 15.9358 14.5468 15.8314C14.8221 15.593 15.1788 15.459 15.8922 15.191L17.7362 14.4981C20.4 13.4973 21.7319 12.9969 21.9667 11.9115C22.2014 10.826 21.1954 9.81905 19.1835 7.80516Z'";
+                    echo "fill='#1C274C' />";
+                    echo "</svg>";
+                    echo "</button>";
+                    echo "</div>";
+                    echo "</div>";
 
-            echo "</post>";
+                    echo "</post>";
 
                 }
             } else {
