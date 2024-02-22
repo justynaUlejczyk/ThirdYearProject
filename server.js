e4b0db04930491a7039e70fc19d324522e9cdb44
bh2013 <91209391+bh2013@users.noreply.github.com>
2024-02-22 17:51:53 +0000

    meetings working with anyone

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-const express = require(`express`)
+const { Socket } = require('engine.io')
+const express = require('express')
+const { disconnect } = require('process')
 const app = express()
-const server = require(`http`).Server(app)
-const io = require(`socket.io`)(server)
-const { v4 } = require('uuid')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4:uuidV4 } = require('uuid')
 
-app.set(`view engine`, `ejs`)
-app.use(express.static(`public`))
-app.get(`/`,(req,res)=>{
-    res.redirect(`/${roomId}`)
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.get('/',(req,res) => {
+    res.redirect(`/${uuidV4()}`)
 })
-app.get(`/:room`,(req,res)=>{
-    res.render(`room`,{roomId: req.params.room})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{ roomId: req.params.room })
+})
+
+
+io.on('connection', socket =>{
+    socket.on('join-room',(roomId,userId)=>{
+        socket.joinRoom(roomId)
+        socket.to(roomId).broadcast.emit('user-connected',userId)
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected',userId)
+        })
+    })
 })
 
 server.listen(3000)
