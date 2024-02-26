ece350a55e889df7c7789c9d7011715b4c61091d
kg2020 <kg2020@hw.ac.uk>
2024-02-26 10:10:24 +0000

    .

--- server.js
+++ server.js
@@ -4,28 +4,28 @@ const { disconnect } = require('process')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4:uuidV4 } = require('uuid')
+const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:room',(req,res)=>{
-    res.render('room',{ roomId: req.params.room })
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
 })
 
 
-io.on('connection', socket =>{
-    socket.on('join-room',(roomId,userId)=>{
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
         socket.joinRoom(roomId)
-        socket.to(roomId).broadcast.emit('user-connected',userId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
 
         socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected',userId)
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
         })
     })
 })
 
-server.listen(3000)
+server.listen(4000)
