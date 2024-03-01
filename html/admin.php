c6b0d823460d8d457d732bd3795fe5cb44a2f97e
Ethan Smyth <es2005@hw.ac.uk>
2024-03-01 16:02:53 +0000

    Add admin page

---
+++ html/admin.php
@@ -0,0 +1,30 @@
+<?php
+require_once "../php/connect_db.php";
+$usersRESULT = pg_query($conn, "SELECT * FROM accounts ORDER BY username ASC");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Username</th>
+            <th>Delete user?</th>
+        </tr>
+        <tr>
+            <?php while ($row = pg_fetch_assoc($usersRESULT)){
+                $username = $row['username'];
+                echo "<tr><td>$username</td> <td><form action='../php/delete_user.php', method='post'><input type='hidden' id='username' name='username' value=$username><input type='submit' value='delete'></form></td></tr>";
+            }
+            ?>
+        </tr>
+
+    </table>
+    
+</body>
+</html>
---
+++ php/delete_user.php
@@ -0,0 +1,5 @@
+<?php
+require_once "connect_db.php";
+$username = $_POST["username"];
+$delete_user = pg_query($conn, "DELETE FROM accounts where username='$username'");
+//header('Location: ../html/admin.php');
