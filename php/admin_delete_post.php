fe4ffa7366f48690df5cc4686ef138d49e244f1c
Ethan Smyth <es2005@hw.ac.uk>
2024-03-18 08:51:22 +0000

    fix typo

--- php/admin_delete_post.php
+++ php/admin_delete_post.php
@@ -1,5 +1,5 @@
 <?php
 require_once "connect_db.php";
 $postid = $_POST["postid"];
-$delete_user = pg_query($conn, "DELETE FROM post where postid='$postid'");
+$delete_post = pg_query($conn, "DELETE FROM post where postid='$postid'");
 //header('Location: ../html/admin.php');
