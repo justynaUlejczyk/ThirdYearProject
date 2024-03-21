79e7fa9b65ce21e8e6d4fd5e57a575fb57af2807
kg2020 <kg2020@hw.ac.uk>
2024-03-21 19:18:49 +0000

    responsive changes

--- html/Home.php
+++ html/Home.php
@@ -42,7 +42,7 @@ $name = pg_fetch_result($userDataRESULT, 0, "name");
         <ul>
             <li>
                 <a href="Profile.php">
-                    <img src="<?php echo "../profile_pic/profile_pic_$username.png";?>" class="nav-profile">
+                    <img src="<?php echo "../profile_pic/profile_pic_$username.png"; ?>" class="nav-profile">
                 </a>
             </li>
 
@@ -59,11 +59,6 @@ $name = pg_fetch_result($userDataRESULT, 0, "name");
                         </svg>
 
                     </button>
-                    <div class="dropdown-content">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                    </div>
                 </div>
             </li>
         </ul>
@@ -103,9 +98,14 @@ $name = pg_fetch_result($userDataRESULT, 0, "name");
                     </a>
                 </li>
                 <li>
-                    <a>
-                        <img src="../images/icons/nav-icons/add-square-svgrepo-com.svg">
-                    </a>
+                    <button onclick="createPost()">
+                        <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22ZM12 8.25C12.4142 8.25 12.75 8.58579 12.75 9V11.25H15C15.4142 11.25 15.75 11.5858 15.75 12C15.75 12.4142 15.4142 12.75 15 12.75H12.75L12.75 15C12.75 15.4142 12.4142 15.75 12 15.75C11.5858 15.75 11.25 15.4142 11.25 15V12.75H9C8.58579 12.75 8.25 12.4142 8.25 12C8.25 11.5858 8.58579 11.25 9 11.25H11.25L11.25 9C11.25 8.58579 11.5858 8.25 12 8.25Z"
+                                fill="#1C274C" />
+                        </svg>
+                    </button>
                 </li>
                 <li>
                     <a href="../html/Group.php">
@@ -196,12 +196,12 @@ $name = pg_fetch_result($userDataRESULT, 0, "name");
                 <li>
                     <div class="dropdown">
                         <img class="nav-profile" onclick="toggleDropdownProfile()"
-                            src="<?php echo "../profile_pic/profile_pic_$username.png";?>">
+                            src="<?php echo "../profile_pic/profile_pic_$username.png"; ?>">
                         </img>
                         <div class="dropdown-content-profile" id="dropdownContentProfile">
                             <div class="dropdown-profile-icon">
                                 <a href="">
-                                <img src="<?php echo "../profile_pic/profile_pic_$username.png";?>" alt="">
+                                    <img src="<?php echo "../profile_pic/profile_pic_$username.png"; ?>" alt="">
                                     <?php echo "<p>$username</p>" ?>
                                 </a>
                             </div>
@@ -271,8 +271,9 @@ $name = pg_fetch_result($userDataRESULT, 0, "name");
             <div>
                 <div>
                     <div id="Banner"></div>
-                    <a href="Profile.php"><img class="pfp" src="<?php echo "../profile_pic/profile_pic_$username.png";?>" alt="pfp"
-                            width="100" height="100" /></a>
+                    <a href="Profile.php"><img class="pfp"
+                            src="<?php echo "../profile_pic/profile_pic_$username.png"; ?>" alt="pfp" width="100"
+                            height="100" /></a>
                     <div id="pfp-outline"></div>
                 </div>
 
@@ -516,9 +517,12 @@ $name = pg_fetch_result($userDataRESULT, 0, "name");
 
                     </div>
                     <div class='comment-create-container'>
-                        <input class='comment-create' id='comment-create-text-<?php echo "$postid"?>' name="text" type="text" required>
-                        <input type="hidden" id='comment-create-postid-<?php echo "$postid"?>' name="postid" value="<?php echo $postid; ?>">
-                        <button name="commentSubmit" onclick='postComment(<?php echo "$postid, \"$username\""?>);'>Comment</button>
+                        <input class='comment-create' id='comment-create-text-<?php echo "$postid" ?>' name="text" type="text"
+                            required>
+                        <input type="hidden" id='comment-create-postid-<?php echo "$postid" ?>' name="postid"
+                            value="<?php echo $postid; ?>">
+                        <button name="commentSubmit"
+                            onclick='postComment(<?php echo "$postid, \"$username\"" ?>);'>Comment</button>
                     </div>
 
                     <?php echo "
