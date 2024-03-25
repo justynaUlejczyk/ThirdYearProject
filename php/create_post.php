04194c7967b02c5f49ab5ec92d19fa7f1c7f1b03
jku2000@hw.ac.uk <jku2000@hw.ac.uk>
2024-03-25 19:02:55 +0000

    header post

--- php/create_post.php
+++ php/create_post.php
@@ -2,7 +2,7 @@
 //session_id("userSession");
 session_start();
 if (! isset($_SESSION["username"])){
-    header('Location: '."../html/login.html"); // redirect the user to the register page if they have not already logged in
+    header('Location: '."../html/login.php"); // redirect the user to the register page if they have not already logged in
 }
 // Connect to the database
 require_once "connect_db.php";
@@ -18,7 +18,7 @@ $tags_trimmed = array_map('trim', $tags_split);
 
 $result = pg_execute($conn, "insert_post", array($username, $text));
     if ($result) {
-    echo "Post created successfully!";
+        header('Location: '."../html/Home.php");
     $postid = pg_fetch_result($result, 0, 'postid');
 
 } else {
@@ -34,7 +34,7 @@ $uploaddir = '../post_images/';
 $uploadfile = $uploaddir . "post_image". $postid . ".png";
 //Add the image associated with the post to the post_images directory with the the number of the post_id linked to the image so it can be found easily
 if(move_uploaded_file($_FILES['post_image']['tmp_name'], $uploadfile)){
-echo "File was successfully uploaded.\n";
+    header('Location: '."../html/Home.php");
 }else{
 echo "Possible file upload attack!\n";
 }
@@ -53,3 +53,6 @@ $notificationResult = pg_execute($conn, "add_notification", array($username, $da
         // Close the connection
         pg_close($conn);
         header('Location: '."../html/Home.php");
+       
+       
+       ?>
