809b8e5071f2305ec140a810f36f03281753b576
kg2020 <kg2020@hw.ac.uk>
2024-03-26 11:44:04 +0000

    change css in group settings

--- html/Profile.php
+++ html/Profile.php
@@ -66,7 +66,7 @@ if (pg_num_rows($userDataRESULT) == 0) {
         <ul>
             <li>
                 <a href="Profile.php">
-                    <img src="../images/icons/Unknown_person.jpg" class="nav-profile">
+                    <img src="<?php echo "../profile_pic/profile_pic_$username.png"; ?>" alt="" class="nav-profile">
                 </a>
             </li>
 
@@ -452,18 +452,11 @@ if (pg_num_rows($userDataRESULT) == 0) {
                                 <span>Comment - $date</span>
                             </div>
                         </div>
-                        <div class='comment-like'>
-                        <button class='like icons' onclick='toggleHeart(this)'>
-                            <svg width='24px' height='24px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                                <path d='M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z' fill='red' />
-                            </svg>
-                        </button>
-                    </div>
+                        
                     </div>
                         <div>
                         <div class='comment-text'>$comment</div>
                         <div class='comment-options'>
-                            <span>1 Like</span>
                             <form action='../php/deleteComments.php' method='post'>
                     <input type='hidden' name='postid' value='$postid'>
                     <input type='hidden' name='timestamp' value='$row[timestamp]'>
